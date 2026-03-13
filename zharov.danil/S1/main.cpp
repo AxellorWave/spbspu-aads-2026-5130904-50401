9a57cc488f744323675469c9f30fc4eef8fa1bec
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-03-13 18:32:39 +0300

    added getSize(), transposeNums(), edit other functions

--- zharov.danil/S1/main.cpp
+++ zharov.danil/S1/main.cpp
@@ -5,15 +5,20 @@
 namespace zharov {
   std::istream & getData(std::istream & in,
     List< std::pair< std::string, List< int > > > & data);
-  std::ostream & printList(std::ostream & out, List< std::pair< std::string,
+  std::ostream & printNums(std::ostream & out, List< List< int> > nums);
+  std::ostream & printNames(std::ostream & out, List< std::pair< std::string,
     List< int > > > data);
+  List< List< int> > transposeNums(List< std::pair< std::string, List< int > > > data);
+  size_t getSize(List< std::pair< std::string, List< int > > > data);
 }
 
 int main()
 {
   zharov::List< std::pair< std::string, zharov::List< int > > > data;
   zharov::getData(std::cin, data);
-  zharov::printList(std::cout, data);
+  zharov::printNames(std::cout, data);
+  auto transpose_nums = zharov::transposeNums(data);
+  zharov::printNums(std::cout, transpose_nums);
 }
 
 std::istream & zharov::getData(std::istream & in,
@@ -32,15 +37,52 @@ std::istream & zharov::getData(std::istream & in,
   return in;
 }
 
-std::ostream & zharov::printList(std::ostream & out, List< std::pair< std::string,
+std::ostream & zharov::printNums(std::ostream & out, List< List< int> > nums)
+{
+  for (auto it = nums.constBegin(); it != nums.constEnd(); ++it) {
+    for (auto inner_it = it->constBegin(); inner_it != it->constEnd(); ++inner_it) {
+      std::cout << *inner_it << ' ';
+    }
+    std::cout << "\n";
+  }
+  return out;
+}
+
+std::ostream & zharov::printNames(std::ostream & out, List< std::pair< std::string,
   List< int > > > data)
 {
   for (auto it = data.constBegin(); it != data.constEnd(); ++it) {
     out << it->first << " ";
-    for (auto num_it = it->second.constBegin(); num_it != it->second.constEnd(); ++num_it) {
-      out << *num_it << " ";
-    }
-    out << "\n";
   }
+  out << "\n";
   return out;
+}
+
+size_t zharov::getSize(List< std::pair< std::string, List< int > > > data)
+{
+  size_t res = 0;
+  for (auto it = data.constBegin(); it != data.constEnd(); ++it) {
+    res = std::max(res, it->second.size());
+  }
+  return res;
+}
+
+zharov::List< zharov::List< int> > zharov::transposeNums(
+  List< std::pair< std::string, List< int > > > data)
+{
+  List< List < int > > res;
+  for (size_t i = 0; i < getSize(data); ++i) {
+    List< int > inner;
+    for (auto it = data.constBegin(); it != data.constEnd(); ++it) {
+      auto num_it = it->second.constBegin();
+      if (it->second.size() > i) {
+        for (size_t j = 0; j < i; ++j) {
+          ++num_it;
+        }
+        inner.pushBack(*num_it);
+      }
+    }
+    res.pushBack(inner);
+  }
+  return res;
 }
