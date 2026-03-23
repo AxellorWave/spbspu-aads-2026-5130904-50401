ee046e844d493d267ea22a22c3e0ee8c5076b676
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-03-23 21:47:07 +0300

    added signature of stack and queue

--- zharov.danil/common/queue.hpp
+++ zharov.danil/common/queue.hpp
@@ -5,7 +5,21 @@
 namespace zharov {
   template< class T >
   class Queue {
+    List< T > list_;
   public:
+    Queue();
+    Queue(const Queue & q);
+    Queue(Queue && q) noexcept;
+    ~Queue() = default;
+    Queue & operator=(const Queue & h);
+    Queue & operator=(Queue && h) noexcept;
+    T & front();
+    const T & front() const;
+    T & back();
+    const T & back() const;
+    bool empty() const;
+    size_t size() const;
+    void clear();
     void push(T rhs);
     T drop();
   };
--- zharov.danil/common/stack.hpp
+++ zharov.danil/common/stack.hpp
@@ -5,7 +5,19 @@
 namespace zharov {
   template< class T >
   class Stack {
+    List< T > list_;
   public:
+    Stack();
+    Stack(const Stack & q);
+    Stack(Stack && q) noexcept;
+    ~Stack() = default;
+    Stack & operator=(const Stack & h);
+    Stack & operator=(Stack && h) noexcept;
+    T & top();
+    const T & top() const;
+    bool empty() const;
+    size_t size() const;
+    void clear();
     void push(T rhs);
     T drop();
   };
