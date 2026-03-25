ed64a0c3aeca4d3b64293575ad3fcd011d750660
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-03-25 16:26:09 +0300

    added signatures of general functionst

---
+++ zharov.danil/S2/general_functions.hpp
@@ -0,0 +1,17 @@
+#ifndef GENERAL_FUNCTIONS_HPP
+#define GENERAL_FUNCTIOINS_HPP
+#include <iostream>
+#include "../common/stack.hpp"
+#include "../common/queue.hpp"
+
+using ll_t = long long;
+
+namespace zharov {
+  bool isOperand(const std::string & o);
+  Queue< std::string > getQueue(const std::string & line);
+  Queue< std::string > getPostfix(Queue< std::string > & infix);
+  ll_t getResult(Queue< std::string > & postfix);
+  int getPriority(const std::string op);
+}
+
+#endif
