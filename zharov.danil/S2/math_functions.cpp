c297ae9c9137d05c98e638e0c4ae9191536ba20c
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-03-25 16:53:49 +0300

    using global constans

--- zharov.danil/S2/math_functions.cpp
+++ zharov.danil/S2/math_functions.cpp
@@ -1,4 +1,3 @@
-#include <limits>
 #include <climits>
 #include <stdexcept>
 #include "math_functions.hpp"
@@ -7,9 +6,7 @@ using ll_t = zharov::ll_t;
 
 ll_t zharov::add(const ll_t & a, const ll_t & b)
 {
-  ll_t max = std::numeric_limits< ll_t >::max();
-  ll_t min = std::numeric_limits< ll_t >::min();
-  if ((b > 0 && a > max - b) || (b < 0 && a < min - b)){
+  if ((b > 0 && a > MAX - b) || (b < 0 && a < MIN - b)){
     throw std::overflow_error("Overflow in add");
   }
   return a + b;
@@ -17,9 +14,7 @@ ll_t zharov::add(const ll_t & a, const ll_t & b)
 
 ll_t zharov::sub(const ll_t & a, const ll_t & b)
 {
-  ll_t max = std::numeric_limits< ll_t >::max();
-  ll_t min = std::numeric_limits< ll_t >::min();
-  if ((b > 0 && a < min + b ) || (b < 0 && a > max + b)) {
+  if ((b > 0 && a < MIN + b ) || (b < 0 && a > MAX + b)) {
     throw std::overflow_error("Overflow in sub");
   }
   return a - b;
@@ -30,15 +25,13 @@ ll_t zharov::mul(const ll_t & a, const ll_t & b)
   if (a == 0 || b == 0) {
     return 0;
   }
-  ll_t max = std::numeric_limits< ll_t >::max();
-  ll_t min = std::numeric_limits< ll_t >::min();
-  if ((a == -1 && b == min) || (b == -1 && a == min)) {
+  if ((a == -1 && b == MIN) || (b == -1 && a == MIN)) {
     throw std::overflow_error("Overflow in mul");
   }
-  if ((a > 0 && b > 0 && a > max / b) ||
-    (a > 0 && b < 0 && b < min / a) || 
-    (a < 0 && b > 0 && a < min / b) ||
-    (a < 0 && b < 0 && a < max / b)) {
+  if ((a > 0 && b > 0 && a > MAX / b) ||
+    (a > 0 && b < 0 && b < MIN / a) ||
+    (a < 0 && b > 0 && a < MIN / b) ||
+    (a < 0 && b < 0 && a < MAX / b)) {
       throw std::overflow_error("Overflow in mul");
     }
   return a * b;
@@ -46,11 +39,11 @@ ll_t zharov::mul(const ll_t & a, const ll_t & b)
 
 ll_t zharov::div(const ll_t & a, const ll_t & b)
 {
-  ll_t min = std::numeric_limits< ll_t >::min();
+  ll_t MIN = std::numeric_limits< ll_t >::min();
   if (b == 0) {
     throw std::logic_error("Div by zero");
   }
-  if (a == min && b == -1) {
+  if (a == MIN && b == -1) {
     throw std::overflow_error("Overflow in div");
   }
   return a / b;
@@ -58,12 +51,10 @@ ll_t zharov::div(const ll_t & a, const ll_t & b)
 
 ll_t zharov::mod(const ll_t & a, const ll_t & b)
 {
-  ll_t min = std::numeric_limits<ll_t>::min();
-
   if (b == 0) {
     throw std::logic_error("Mod by zero");
   }
-  if (a == min && b == -1) {
+  if (a == MIN && b == -1) {
     throw std::overflow_error("Overflow in mod");
   }
   return a % b;
@@ -71,7 +62,6 @@ ll_t zharov::mod(const ll_t & a, const ll_t & b)
 
 ll_t zharov::bitShiftLeft(const ll_t & a, const ll_t & b)
 {
-  ll_t max = std::numeric_limits< ll_t >::max();
   ll_t width = sizeof(ll_t) * CHAR_BIT;
   if (a < 0) {
     throw std::overflow_error("Cannot shift negative number");
@@ -79,7 +69,7 @@ ll_t zharov::bitShiftLeft(const ll_t & a, const ll_t & b)
   if (b < 0 || b >= width) {
     throw std::overflow_error("Invalid shift amount");
   }
-  if (a > (max >> b)) {
+  if (a > (MAX >> b)) {
     throw std::overflow_error("Overflow in bitShiftLeft");
   }
   return a << b;
