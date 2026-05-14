2fdc52ad7f5490eb727f7858dd8a980ab4c779a9
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-05-14 22:19:04 +0300

    apdate signature

--- zharov.danil/S3/hash_table.hpp
+++ zharov.danil/S3/hash_table.hpp
@@ -1,17 +1,52 @@
 #ifndef HASH_TABLE_HPP
 #define HASH_TABLE_HPP
+#include <cstddef>
 
 namespace zharov
 {
+  enum class State
+  {
+    EMPTY,
+    OCCUPIED,
+    TOMBSTONE
+  };
+
+  template < class Key, class Value >
+  struct Slot
+  {
+    Key key_;
+    Value value_;
+  };
+
   template < class Key, class Value, class Hash, class Equal >
   class HashTable
   {
   public:
-    ... void add(Key k, Value v);
+    HashTable();
+    HashTable(size_t capacity);
+    HashTable(const HashTable& table);
+    HashTable(HashTable&& table) noexcept;
+    ~HashTable();
+
+    HashTable& operator=(const HashTable& table);
+    HashTable& operator=(HashTable&& table) noexcept;
+
+    void add(Key k, Value v);
     Value drop(Key k);
     bool has(Key k);
     void rehash(size_t slots);
-    ...
+    size_t getSize() const;
+    size_t getCapacity() const;
+    Value& at(Key key);
+    const Value& at(Key key) const;
+
+  private:
+    Hash hasher_;
+    Equal comparator_;
+    State* states_;
+    Slot< Key, Value >* sllots_;
+    size_t capacity_;
+    size_t size_;
   };
 }
 
