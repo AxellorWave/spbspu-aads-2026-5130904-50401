a28d7c65faaee2ac55f06c3b16db20b58819c50d
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-05-14 19:29:59 +0300

    added signature of HashTable

---
+++ zharov.danil/S3/hash_table.hpp
@@ -0,0 +1,18 @@
+#ifndef HASH_TABLE_HPP
+#define HASH_TABLE_HPP
+
+namespace zharov
+{
+  template < class Key, class Value, class Hash, class Equal >
+  class HashTable
+  {
+  public:
+    ... void add(Key k, Value v);
+    Value drop(Key k);
+    bool has(Key k);
+    void rehash(size_t slots);
+    ...
+  };
+}
+
+#endif
