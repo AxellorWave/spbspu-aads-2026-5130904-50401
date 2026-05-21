a45ac797e7a8aaa2a303e9a8291d86f37651dcf7
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-05-21 20:52:38 +0300

    push

--- zharov.danil/S4/bstree.hpp
+++ zharov.danil/S4/bstree.hpp
@@ -34,9 +34,12 @@ namespace zharov
     void swap(BSTree& other) noexcept;
     void clear() noexcept;
 
-    void push(Key k, Value v);
-    Value get(Key k);
-    Value drop(Key k);
+    void push(const Key& k, const Value& v);
+    void push(Key&& k, Value&& v);
+    Value get(const Key& k);
+    Value& at(const Key& k);
+    const Value& at(const Key& k) const;
+    Value drop(const Key& k);
     const_iterator rotateLeft(const_iterator it);
     const_iterator rotateRight(const_iterator it);
     const_iterator rotateLargeLeft(const_iterator it);
@@ -53,19 +56,22 @@ namespace zharov
     detail::Node< Key, Value >* BSTree< Key, Value, Compare >::clone(
       detail::Node< Key, Value >* root,
       detail::Node< Key, Value >* parent);
-    void BSTree< Key, Value, Compare >::deleteNodes(detail::Node< Key, Value >* node) noexcept
+    void BSTree< Key, Value, Compare >::deleteNodes(detail::Node< Key, Value >* node) noexcept;
+    template < class K, class V >
+    void pushImpl(K&& k, V&& v);
   };
 }
 
 template < class Key, class Value, class Compare >
 zharov::detail::Node< Key, Value >* zharov::BSTree< Key, Value, Compare >::initFake()
 {
-  if (detail::Node< Key, Value >::fake == nullptr)
+  using Node = detail::Node< Key, Value >;
+  if (Node::fake == nullptr)
   {
-    detail::Node< Key, Value >::fake = new detail::Node< Key, Value >(Key(), Value(), nullptr);
-    detail::Node< Key, Value >::fake->left_ = etail::Node< Key, Value >::fake;
-    detail::Node< Key, Value >::fake->right_ = etail::Node< Key, Value >::fake;
-    detail::Node< Key, Value >::fake->parent_ = etail::Node< Key, Value >::fake;
+    Node::fake = new detail::Node< Key, Value >(Key(), Value(), nullptr);
+    Node::fake->left_ = etail::Node< Key, Value >::fake;
+    Node::fake->right_ = etail::Node< Key, Value >::fake;
+    Node::fake->parent_ = etail::Node< Key, Value >::fake;
   }
   return detail::Node< Key, Value >::fake;
 }
@@ -139,12 +145,12 @@ zharov::detail::Node< Key, Value >* zharov::BSTree< Key, Value, Compare >::clone
   detail::Node< Key, Value >* root,
   detail::Node< Key, Value >* parent)
 {
+  using Node = detail::Node< Key, Value >;
   if (root->isFake())
   {
-    return detail::Node< Key, Value >::fake;
+    return Node::fake;
   }
-  detail::Node< Key, Value >* node =
-    new detail::Node< Key, Value >(root->key_, root->value_, parent);
+  Node* node = new Node(root->key_, root->value_, parent);
   node->left_ = clone(root->left_, node);
   node->right_ = clone(root->right_, node);
   return node;
@@ -172,6 +178,62 @@ zharov::BSTree< Key, Value, Compare >& zharov::BSTree< Key, Value, Compare >::op
     swap(copy);
   }
   return *this;
+}
+
+template < class Key, class Value, class Compare >
+template < class K, class V >
+void zharov::BSTree< Key, Value, Compare >::pushImpl(K&& k, V&& v)
+{
+  using Node = detail::Node< Key, Value >;
+
+  if (root_->isFake())
+  {
+    root_ = new Node(std::forward< K >(k), std::forward< V >(v), Node::fake);
+    ++size_;
+    return;
+  }
+
+  Node* cur = root_;
+  while (true)
+  {
+    if (comp_(k, cur->key_))
+    {
+      if (cur->left_->isFake())
+      {
+        cur->left_ = new Node(std::forward< K >(k), std::forward< V >(v), cur);
+        ++size_;
+        return;
+      }
+      cur = cur->left_;
+    }
+    else if (comp_(cur->key_, k))
+    {
+      if (cur->right_->isFake())
+      {
+        cur->right_ = new Node(std::forward< K >(k), std::forward< V >(v), cur);
+        ++size_;
+        return;
+      }
+      cur = cur->right_;
+    }
+    else
+    {
+      cur->value_ = std::forward< V >(v);
+      return;
+    }
+  }
+}
+
+template < class Key, class Value, class Compare >
+void zharov::BSTree< Key, Value, Compare >::push(const Key& k, const Value& v)
+{
+  pushImpl(k, v);
+}
+
+template < class Key, class Value, class Compare >
+void zharov::BSTree< Key, Value, Compare >::push(Key&& k, Value&& v)
+{
+  pushImpl(std::move(k), std::move(v));
 }
 
 #endif
