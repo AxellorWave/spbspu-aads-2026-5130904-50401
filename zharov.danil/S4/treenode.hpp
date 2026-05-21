790acaff1e5fd4ca38bae41a27c1e94f5c7fb31c
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-05-21 15:24:00 +0300

    declaration of Node struct

---
+++ zharov.danil/S4/treenode.hpp
@@ -0,0 +1,28 @@
+#ifndef TREENODE_HPP
+#define TREENODE_HPP
+
+namespace zharov
+{
+  namespace detail
+  {
+
+    template < class Key, class Value >
+    struct Node
+    {
+      Key key_;
+      Value value_;
+      Node< Key, Value > left_;
+      Node< Key, Value > right_;
+      Node< Key, Value > parent_;
+
+      static Node< Key, Value >* fake;
+
+      Node(const Key& key, const Value& val, Node< Key, Value > parent);
+      Node(Key&& key, Value&& val, Node< Key, Value > parent);
+
+      bool isFake() const;
+    };
+  }
+}
+
+#endif
