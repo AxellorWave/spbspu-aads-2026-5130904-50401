9d5f8b49f53c29a8fc467218cfa8bce98db9ef7b
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-05-21 16:33:35 +0300

    treenote realization

--- zharov.danil/S4/treenode.hpp
+++ zharov.danil/S4/treenode.hpp
@@ -10,18 +10,44 @@ namespace zharov
     {
       Key key_;
       Value value_;
-      Node< Key, Value > left_;
-      Node< Key, Value > right_;
-      Node< Key, Value > parent_;
+      Node< Key, Value >* left_;
+      Node< Key, Value >* right_;
+      Node< Key, Value >* parent_;
 
       static Node< Key, Value >* fake;
 
-      Node(const Key& key, const Value& val, Node< Key, Value > parent);
-      Node(Key&& key, Value&& val, Node< Key, Value > parent);
+      Node(const Key& key, const Value& val, Node< Key, Value >* parent);
+      Node(Key&& key, Value&& val, Node< Key, Value >* parent);
 
       bool isFake() const;
     };
   }
+}
+
+template < class Key, class Value >
+zharov::detail::Node< Key, Value >::Node(const Key& key,
+  const Value& val,
+  Node< Key, Value >* parent):
+  key_(key),
+  value_(val),
+  left_(fake),
+  right_(fake),
+  parent_(parent)
+{}
+
+template < class Key, class Value >
+zharov::detail::Node< Key, Value >::Node(Key&& key, Value&& val, Node< Key, Value >* parent):
+  key_(std::move(key)),
+  value_(std::move(val)),
+  left_(fake),
+  right_(fake),
+  parent_(parent)
+{}
+
+template < class Key, class Value >
+bool zharov::detail::Node< Key, Value >::isFake() const
+{
+  return this == fake;
 }
 
 #endif
