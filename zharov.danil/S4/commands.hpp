1667588ebf4df5dc5a7f92fab4bcf6fc193e7a11
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-05-23 20:50:44 +0300

    main and commands.hpp

---
+++ zharov.danil/S4/commands.hpp
@@ -0,0 +1,12 @@
+#ifndef COMMANDS_HPP
+#define COMMANDS_HPP
+#include <bstree.hpp>
+
+namespace zharov
+{
+  using dictionary_t = zharov::BSTree< int, std::string, std::less< int > >;
+  using dictionaries_t = zharov::BSTree< std::string, dictionary_t, std::less< std::string > >;
+  using cmd = void (*)(std::ostream&, std::istream&, dictionaries_t&);
+}
+
+#endif
