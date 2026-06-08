d5b1b719dd9e5a8e214a9f2518fe9f9c45687711
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-06-08 17:31:01 +0300

    std::forward <- std::move

--- zharov.danil/common/queue.hpp
+++ zharov.danil/common/queue.hpp
@@ -64,7 +64,7 @@ void zharov::Queue< T >::push(const T& rhs)
 template < class T >
 void zharov::Queue< T >::push(T&& rhs)
 {
-  list_.pushBack(std::move(rhs));
+  list_.pushBack(std::forward< T >(rhs));
 }
 
 template < class T >
