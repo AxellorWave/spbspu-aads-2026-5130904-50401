bc86c2b7ab1a5bdf69408cbe82bb41fd0ebf8604
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-06-18 16:18:11 +0300

    exception msg

--- zharov.danil/F0/main.cpp
+++ zharov.danil/F0/main.cpp
@@ -108,7 +108,7 @@ int main(int argc, char** argv)
     catch (const std::exception& e)
     {
       std::cin.clear();
-      std::cout << "<INVALID COMMAND>\n";
+      std::cout << "<INVALID COMMAND" << e.what() << ">\n";
       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
   }
